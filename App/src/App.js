65028143c385ccab3c18801668c644acac170b5f
Elane Souza <104568689+ElaneSz@users.noreply.github.com>
2023-09-19 14:13:12 +0000

    Aula do dia 19.09

--- App/src/App.js
+++ App/src/App.js
@@ -1,16 +1,24 @@
 import React from 'react';
-import { SafeAreaView } from 'react-native';
+import { SafeAreaView, StyleSheet } from 'react-native';
 
 import Primeiro from './Components/Primeiro';
 import Comp, {Comp2, Comp3} from './Components/Multiplos';
 
 export default function App(){
   return(
-        <SafeAreaView>
+        <SafeAreaView style={style.App}>
           <Primeiro/>
           <Comp/>
           <Comp2/>
           <Comp3/>
         </SafeAreaView>
-  );
+  )
+
+  const style = StyleSheet.creat ({
+    App:{
+      flexGrow: 1,
+      justifyContent: "center",
+      alignItems: "center"
+    },
+  })
 }
