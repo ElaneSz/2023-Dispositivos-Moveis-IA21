5590409d946bc4d76b8eee12d23fb108751bfd64
Elane Souza <104568689+ElaneSz@users.noreply.github.com>
2023-10-01 20:47:05 +0000

    Sorteador de número | Finalizado

--- App/src/Components/Aleatorio.js
+++ App/src/Components/Aleatorio.js
@@ -1,15 +1,36 @@
-import React from "react";
-import { Text } from "react-native";
-import estilo from "../estilo";
+import React, { useState, useEffect } from 'react';
+import { Text } from 'react-native';
+
+const SorteadorNumeros = () => {
+  const [numerosSorteados, setNumerosSorteados] = useState([]);
+
+  useEffect(() => {
+    const gerarNumerosAleatorios = () => {
+      const novosNumeros = [];
+      while (novosNumeros.length < 6) {
+        const novoNumero = Math.floor(Math.random() * 60) + 1;
+        if (!novosNumeros.includes(novoNumero)) {
+          novosNumeros.push(novoNumero);
+        }
+      }
+      setNumerosSorteados(novosNumeros);
+    };
+
+    gerarNumerosAleatorios();
+  }, []);
+
+  return (
+    <>
+      <Text>Números sorteados:</Text>
+      <Text>{numerosSorteados.join(', ')}</Text>
+    </>
+  );
+};
+
+export default SorteadorNumeros;
 
 
-let numeroAleatorio = Math.floor(Math.random() * 60)
-
-export default aleatorio => (
-    <Text style={ estilo.fontG } >Valor {numeroAleatorio}</Text> 
-);
 
 
-export {aleatorio}
 
 //let numeroAleatorio = Math.floor(Math.random() * todasFrases [categoriaSelecionada].frases.length)
