e4718f42f11792ae589f8f4f781982ef580b83df
Elane Souza <104568689+ElaneSz@users.noreply.github.com>
2023-10-10 17:54:51 +0000

    Aula do dia 10.10 | Desenvolvimento de um contador

--- App/src/App.js
+++ App/src/App.js
@@ -1,29 +1,12 @@
 import React from 'react';
 import { SafeAreaView, StyleSheet } from 'react-native';
-import Titulo from './Components/Titulo';
-import Botao from './Components/Botao'
-{/*import Comp, { Comp2, Comp3 } from './Components/Multiplos';
-import Primeiro from './Components/Primeiro';
-import MinMax from './Components/MinMax';
-import Aleatorio from './Components/Aleatorio';*/}
+import Contador from './Components/Contador';
+
 
 export default function App() { 
   return (
     <SafeAreaView style={styles.container}>
-      {/*<MinMax min={2} max={20}/>
-      <MinMax min={20} max={100}/>
-      <Aleatorio min={1} max={60} ></Aleatorio>
-      <Aleatorio min={1} max={60} ></Aleatorio>
-      <Aleatorio min={1} max={60} ></Aleatorio>
-      <Aleatorio min={1} max={60} ></Aleatorio>
-      <Aleatorio min={1} max={60} ></Aleatorio>
-      <Aleatorio min={1} max={60} ></Aleatorio>
-      <Comp />
-      <Comp2 />
-      <Comp3 />
-      <Primeiro />*/}
-      <Botao/>
-      <Titulo principal="titulo" secundario="subtitulo"></Titulo>
+      <Contador inicial={100} passo={2}/>
     </SafeAreaView>
   );
 }
