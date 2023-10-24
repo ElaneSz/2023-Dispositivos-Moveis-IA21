5683bf31cf20e965c08efff4fc36e776b8ecece2
Elane Souza <104568689+ElaneSz@users.noreply.github.com>
2023-10-24 18:08:41 +0000

    Aula do dia 24.10

--- App/src/App.js
+++ App/src/App.js
@@ -1,12 +1,40 @@
 import React from 'react';
 import { SafeAreaView, StyleSheet } from 'react-native';
-import Contador from './Components/Contador';
 
+{/*import Comp, { Comp2, Comp3 } from './Components/Multiplos';
+import Primeiro from './Components/Primeiro';
+import MixMax from './Components/MixMax';
+import Aleatorio from './Components/Aleatorio';
+import Botao from './Components/Botao';
+import Titulo from './Components/Titulo';*/}
+
+//import Contador from './Components/Contador';
+
+import Pai from './Components/Indireta/Pai'
 
 export default function App() { 
   return (
     <SafeAreaView style={styles.container}>
+      
+      <Pai/>
+      
+      {/*<Titulo principal="Titulo" secundario="Subtitulo"/>
       <Contador inicial={100} passo={2}/>
+      <Contador/>
+      <Botao/>
+      <Aleatorio min={1} max={60}/>
+    <Aleatorio min={1} max={60}/>
+    <Aleatorio min={1} max={60}/>
+    <Aleatorio min={1} max={60}/>
+    <Aleatorio min={1} max={60}/>
+    <Aleatorio min={1} max={60}/>
+      <MixMax min={2} max={20 }/>
+      <MixMax min={15} max={65 }/>
+      
+      <Comp />
+      <Comp2 />
+      <Comp3 />
+  <Primeiro />*/}
     </SafeAreaView>
   );
 }
@@ -16,6 +44,5 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#DDA0DD',
   },
 });
