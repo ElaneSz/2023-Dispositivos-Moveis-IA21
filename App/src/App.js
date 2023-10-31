fc85a9b9bc94cf039365140e00ea61f25ea24a63
Elane Souza <104568689+ElaneSz@users.noreply.github.com>
2023-10-31 18:09:12 +0000

    Aula do dia 31.10

--- App/src/App.js
+++ App/src/App.js
@@ -1,20 +1,13 @@
 import React from 'react';
 import { SafeAreaView, StyleSheet } from 'react-native';
-import ParImpar from './Components/ParImpar';
-import Familia from './Components/Relacao/Familia';
-import Membros from './Components/Relacao/Membros';
+import UsuarioLogado from './Components/UsuarioLogado';
+import listaProdutosV2 from './Components/Produtos/listaProdutosV2';
+import ListaProdutosV2 from './Components/Produtos/listaProdutosV2';
 
 export default function App() { 
   return (
     <SafeAreaView style={styles.container}>    
-      <Familia>
-        <Membros nome="Elane" sobrenome="Souza" />
-        <Membros nome="Emilly" sobrenome="Souza" />
-        <Membros nome="Emerson" sobrenome="Souza" />
-      </Familia>
-      <Familia>
-        <Membros nome="Juliana" sobrenome="Lobato" />
-      </Familia>
+      <ListaProdutosV2/>
     </SafeAreaView>
   );
 }
