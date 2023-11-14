b62a6835476c1c07f4eb0f99e46a081735de6f25
Elane Souza <104568689+ElaneSz@users.noreply.github.com>
2023-11-14 18:04:24 +0000

    Aula do dia 14.11

--- App/src/Components/Mega/Mega.js
+++ App/src/Components/Mega/Mega.js
@@ -1,30 +1,54 @@
-import { React, Component } from "react";
-import { Button, Text, TextInput } from "react-native";
-import estilo from "../../estilo";
+import React, { Component } from "react"
+import { Button, Text, TextInput, View } from 'react-native'
+import estilo from '../../estilo'
+
+import NumeroMega from "./numeroMega"
+import numeroMega from "./numeroMega"
 
 export default class Mega extends Component {
+
     state = {
-        qtdeNumeros: this.props.qtdeNumeros, // Pega a propriedade correta
+        qtdeNumeros: this.props.qtdeNumeros,
         numeros: []
     }
 
-    alterarqtdeNumero = (qtde) => {
+    alterarQtdeNumero = (qtde) => {
         this.setState({ qtdeNumeros: +qtde })
     }
 
-    gerarNumerosNaoContido = nums => {
+    gerarNumeroNaoContido = nums =>{
         const novo = parseInt(Math.random()*60)+1
-        return nums.includes(novo) ? this.gerarNumerosNaoContido(nums) : novo
+        return nums.includes(novo) ? this.gerarNumeroNaoContido(nums) : novo
     }
 
-    gerarNumeros = () => {
+   gerarNumeros = () =>{
         const numeros = Array (this.state.qtdeNumeros)
         .fill()
-        .reduce(n => [...n, this.gerarNumerosNaoContido(n)], [])
+        .reduce(n=> [...n, this.gerarNumeroNaoContido(n)], [])
         this.setState({numeros})
     }
 
-    render (){
+    /*gerarNumeros = ( ) =>{
+        const {qtdeNumeros} = this.state
+        const numeros = []
+
+        for(let i = 0; i< qtdeNumeros; i++){
+            const n = this.gerarNumeroNaoContido(numeros)
+            numeros.push(n)
+        }
+
+        numeros.sort((a,b) => a-b)
+        this.setState({numeros})
+    }*/
+
+        exibirNumeros = () =>{
+            const nums = this.state.numeros
+            return nums.map(num => {
+                return <NumeroMega key={num} num={num}/>
+            })
+        }
+
+    render() {
         return (
             <>
                 <Text style={estilo.fontG}>
@@ -32,15 +56,22 @@ export default class Mega extends Component {
                 </Text>
                 <TextInput
                     keyboardType={'numeric'}
-                    style={{borderBottomWidth:1}}
-                    placeholder="Quantidade de números:"
+                    style={{borderBottomWidth: 1}}
+                    placeholder="Qtde de Números"
                     value={this.state.qtdeNumeros}
-                    onChangeText={this.alterarqtdeNumero}>
-                </TextInput>
-                <Button title="Gerar" onPress={this.gerarNumeros}/>
-                <Text>
-                    {this.state.numeros.join(',')}
-                </Text>
+                    onChangeText={this.alterarQtdeNumero}
+                />
+                <Button title='Gerar' 
+                    onPress={this.gerarNumeros}
+                />
+                <View style={{
+                    marginTop: 20, 
+                    flexDirection: 'row',
+                    flexWrap: 'wrap',
+                    justifyContent:'center'
+                }}>
+                    {this.exibirNumeros()}
+                </View>
             </>
         )
     }
